7ffe04e71a900be3e0af0985c0712a6664ed213d
Niko <197581864+Nikotheabstract@users.noreply.github.com>
2025-06-15 10:37:11 +0300

    almost ready

--- javascript.js
+++ javascript.js
@@ -1,46 +1,108 @@
 const input = document.getElementById("input");
 
+
 let num = "";
 let nextNum = "";
 let operator = "";
 
+
 function add (num, nextNum) {
-return num + nextNum;
+    let result = num + nextNum;
+    return Math.round(result * 1000) / 1000;
 }
 
 function subtract (num, nextNum) {
-    return num - nextNum;
+    let result = num - nextNum;
+    return Math.round(result * 1000) / 1000;
 }
 
 function multiply (num, nextNum) {
-    return num * nextNum;
+    let result = num * nextNum;
+    return Math.round(result * 1000) / 1000;
 }
 
 function divide (num, nextNum) {
+  let result = num / nextNum;  
     if (nextNum === 0){
-        return alert("Error! You can't devide by zero");
+        alert("Error! You can't devide by zero");
+        return "Error!"
     } else{
-    return num / nextNum;
+    return Math.round(result * 1000) / 1000;
     }
 }
-
-function operate (num, operator, nextNum) {
+let operate = function (num, operator, nextNum,) {
     num = Number(num);
     nextNum = Number(nextNum);
-    let result = ""
+
     switch(operator){
         case "+":
-          result = add(num, nextNum);
+          return add(num, nextNum);
         break;
         case "-": 
-          result = subtract(num, nextNum);
+          return subtract(num, nextNum);
         break;
         case "*": 
-          result = multiply(num, nextNum);
+          return multiply(num, nextNum);
         break;
         case "/": 
-          result = divide(num, nextNum);
+          return divide(num, nextNum);
         break;
-    }
-    return result;
+    };
+    num = ""
+    nextNum = ""
+
 }
+
+const numbers = document.querySelectorAll('.number')
+
+
+let ifTheFirst = true;
+
+numbers.forEach(button => {
+    button.addEventListener("click", () => { 
+      if (ifTheFirst) {  
+        num += button.textContent; 
+        input.textContent = num;
+      } else {
+        nextNum += button.textContent;
+        input.textContent = nextNum; 
+      }
+    });
+  });
+
+const op = document.querySelectorAll('.op:not(.equals)');
+
+op.forEach(button => {
+    button.addEventListener("click", () => {
+        operator = button.textContent;
+        ifTheFirst = false;
+        
+    });
+});        
+
+const equals = document.querySelector('.equals');
+
+equals.addEventListener("click", () => {
+    input.textContent = operate(num, operator, nextNum);
+    num = input.textContent;
+    nextNum = "";
+    ifTheFirst = true;
+});
+
+const AC = document.querySelector('.ac');
+
+AC.addEventListener('click', () => {
+    input.textContent = "";
+    num = "";
+    nextNum = "";
+});
+
+const DEL = document.querySelector('.del');
+
+DEL.addEventListener('click', () => {
+    let outcome = input.textContent;
+    input.textContent = outcome.slice(0, -1);
+})
+
+const dot = document.querySelector('.dot');
+
